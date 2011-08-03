4c904564aa38de983a7aa797e07eaae9f1c306ad
Neeraj Singh <neerajdotname@gmail.com>
2011-08-03 00:41:02 -0400

    delete extra models

--- app/models/models/product.rb
+++
@@ -1,8 +0,0 @@
-class Product < ActiveRecord::Base
-  validates_presence_of :name, :description, :price
-  
-  has_attached_file :picture, :styles => { :original => ["420x420#", :jpg], :thumbnail => ["190x190#", :jpg] }
-
-  scope :active, where(:active => true)
-
-end
