393864dfea88438c8785bdfeafe307c8cc79f310
Neeraj Singh <neerajdotname@gmail.com>
2011-08-07 15:50:58 -0400

    done

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,19 +1,35 @@
 $(function(){
 
+  window.Product = Backbone.Model.extend({
+    defaults: {name: 'name missing'},
+
+    initialize: function(){}
+  });
+
+  window.ProductList = Backbone.Collection.extend({
+    model: Product,
+    localStorage: new Store('backbonecart')
+  });
+
+  window.Products = new window.ProductList;
+
   window.AppView = Backbone.View.extend({
     el: $("#products"),
 
     initialize: function(){
+      Products.create({name: 'iphone4'});
       Products.bind('reset', this.addAll, this);
       Products.fetch();
     },
 
     addOne: function(product){
+      console.log('addOne is called');
       var view = new ProductView({model: product});
       this.$('#products-list').append(view.render().el);
     },
 
     addAll: function(){
+      console.log('addAll is called');
       Products.each(this.addOne);
     }
   });
